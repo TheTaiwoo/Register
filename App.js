f7cfdd2c234629d334420e45067785fc8abaa820
Taiwo Talabi <97107272+TheTaiwoo@users.noreply.github.com>
2024-04-26 22:56:50 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Form from "./Form";
+
+var userIsRegistered = false;
+
+function App() {
+    return(
+        <div>
+            <Form />
+        </div>
+    );
+}
+
+export default App;
---
+++ Form.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Form() {
+    return(
+             <form>
+              <input type="text" placeholder="Username" />
+              <input type="password" placeholder="Password" />
+              <input type="password" placeholder="Confirm Password" />
+              <button type="submit">Register</button>
+            </form>
+    );
+}
+export default Form;
